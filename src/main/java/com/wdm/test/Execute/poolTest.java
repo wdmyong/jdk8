23d684aa13ea363e8c90908e6d7c2a839caab7d8
duanyong <duanyong@kuaishou.com>
2017-07-19 20:21:21 +0800

    add execute and apache http

---
+++ src/main/java/com/wdm/test/Execute/poolTest.java
@@ -0,0 +1,20 @@
+package com.wdm.test.Execute;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by duanyong on 2017/7/19.
+ */
+public class poolTest {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("test");
+            }
+        });
+        executorService.execute(() -> System.out.println("jdk8 test"));
+    }
+}
